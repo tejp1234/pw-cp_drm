1d76fc8510d84550c2931fd99f6eafa6cd6e11e1
kingrajashish <54197173+kingrajashish@users.noreply.github.com>
2025-04-26 17:46:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "29538829"
+api_hash = "0098baaaa11b512a0a8b8eaf33a4d91a"
+bot_token = "7832589970:AAHWWZQaoQjzS7amVk9-shpyf2NwP0nQnBY"
+auth_users = [1118716436]
+sudo_users = [1118716436]
