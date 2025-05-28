facd515a0d22532347a5b5650d00c87c9443ab7f
kingrajashish <54197173+kingrajashish@users.noreply.github.com>
2025-05-28 13:50:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "29538829"
 api_hash = "0098baaaa11b512a0a8b8eaf33a4d91a"
 bot_token = "7832589970:AAHWWZQaoQjzS7amVk9-shpyf2NwP0nQnBY"
-auth_users = [1118716436]
+auth_users = [1118716436,6443740402]
 sudo_users = [1118716436]
